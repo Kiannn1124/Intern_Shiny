5aab7caae8e86a1c0205954b4779a82e4c3347ac
Wei Kian Ng <weikian.ng@n-actuarial.com>
2023-04-07 18:13:49 +0800

    Created new UI code

--- R script (Shiny).R
+++ R script (Shiny).R
@@ -1,5 +1,16 @@
 library(shiny)
-library(readxl)
-read=read_excel("D:/Intern (R project)/R Shiny Assignment.xlsx")
-library(shinydashboard)
-UI=dashboardPage(dashboardHeader(title="Wei Kian's Shiny App"),dashboardSidebar())
+library(ggplot2)
+
+ui=fluidPage(sidebarPanel(theme = "united",
+             fileInput("file", "Upload Your CSV file here"),
+             sliderInput("TF", "Tail Factor", value=1.1, min=0, max=10, step=0.1)),
+             mainPanel(tableOutput("table"),
+             plotOutput("plot")))
+
+server=function(input, output, session){
+  output$
+}
+  
+
+
+shinyApp(ui, server)
