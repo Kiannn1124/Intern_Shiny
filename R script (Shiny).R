0b11fcb4ac1d653b9f22f4610882ede0bb021c14
Wei Kian Ng <weikian.ng@n-actuarial.com>
2023-04-07 19:04:55 +0800

    ...

--- R script (Shiny).R
+++ R script (Shiny).R
@@ -4,8 +4,8 @@ library(ggplot2)
 ui=fluidPage(sidebarPanel(theme = "united",
              fileInput("file", "Upload Your CSV file here"),
              sliderInput("TF", "Tail Factor", value=1.1, min=0, max=10, step=0.1)),
-             tabsetPanel(tabPanel("Cumulative Paid Claims ($) - Table",tableOutput("table"),
-             tabPanel("Cumulative Paid Claims ($) - Graph",plotOutput("plot")))))
+             tabsetPanel(tabPanel("Cumulative Paid Claims ($) - Table",tableOutput("table")),
+             tabPanel("Cumulative Paid Claims ($) - Graph",plotOutput("plot"))))
 
 server=function(input, output, session){
   output$plot=renderPlot({
